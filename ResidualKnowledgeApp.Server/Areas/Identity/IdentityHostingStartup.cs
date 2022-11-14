9cec3a5bb76b29775b3cb22359c5bcfb7d1f9dab
Dmitriy Kuznetsov <dmitrvlkuznetsov@gmail.com>
2022-11-14 23:50:47 +0300

    Added Identity

---
+++ ResidualKnowledgeApp.Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ResidualKnowledgeApp.Server.Areas.Identity.Data;
+using ResidualKnowledgeApp.Server.Data;
+
+[assembly: HostingStartup(typeof(ResidualKnowledgeApp.Server.Areas.Identity.IdentityHostingStartup))]
+namespace ResidualKnowledgeApp.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ResidualKnowledgeAppServerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ResidualKnowledgeAppServerContextConnection")));
+
+                services.AddDefaultIdentity<ResidualKnowledgeAppServerUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ResidualKnowledgeAppServerContext>();
+            });
+        }
+    }
+}
